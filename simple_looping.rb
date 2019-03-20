797f3ef5f8487ac1e602c2f39ab34c3781edf610
Conor Owens <conorj.owens@gmail.com>
2019-03-20 05:36:38 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,13 +12,15 @@ def loop_iterator(number_of_times)
  counter = 0
   loop do
     counter += 1
-    puts 
+    puts phrase
+    break if counter == number_of_times
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times.times do
+    
 end
 
 def while_iterator(number_of_times)
